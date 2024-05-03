a51b53dcc91d059842968d6b497da38076cd6b7e
Tom COLEGNO <tomcolegno@Toms-Laptop.local>
2024-05-03 14:21:10 +0200

    add title in the middle

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import './App.css'
 import Card from './components/Card/Card';
+import Title from './components/Title/Title';
 
 function App() {
-  return (<><Card /></>);
+  return (<>
+    <Title text="Jeu de Mémoire" />
+    <Card />
+  </>);
 }
 
 export default App;
--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,27 +1,27 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import Button from '../Button/Button';
 
 function Card() {
     // Définition des données des cartes avec les URL des images à afficher
     const cardsData = [
-        { id: 1, value: 'images/leo1.png' },
-        { id: 2, value: 'images/leo2.png' },
-        { id: 3, value: 'images/leo3.png' },
-        { id: 4, value: 'images/leo4.png' },
-        { id: 5, value: 'images/leo5.png' },
-        { id: 6, value: 'images/leo6.png' },
-        { id: 7, value: 'images/leo7.png' },
-        { id: 8, value: 'images/leo8.png' },
-        { id: 9, value: 'images/leo9.png' },
-        { id: 10, value: 'images/leo10.png'},
-        { id: 11, value: 'images/leo11.png' },
-        { id: 12, value: 'images/leo12.png' },
-        { id: 13, value: 'images/leo13.png'},
-        { id: 14, value: 'images/leo14.png' },
-        { id: 15, value: 'images/leo15.png' },
-        { id: 16, value: 'images/leo16.png'},
-        { id: 17, value: 'images/leo17.png' },
-        { id: 18, value: 'images/leo18.png'},
+        { id: 1, value: 'public/assets/images/leo1.png' },
+        { id: 2, value: 'public/assets/images/leo2.png' },
+        { id: 3, value: 'public/assets/images/leo3.png' },
+        { id: 4, value: 'public/assets/images/leo4.png' },
+        { id: 5, value: 'public/assets/images/leo5.png' },
+        { id: 6, value: 'public/assets/images/leo6.png' },
+        { id: 7, value: 'public/assets/images/leo7.png' },
+        { id: 8, value: 'public/assets/images/leo8.png' },
+        { id: 9, value: 'public/assets/images/leo9.png' },
+        { id: 10, value: 'public/assets/images/leo10.png'},
+        { id: 11, value: 'public/assets/images/leo11.png' },
+        { id: 12, value: 'public/assets/images/leo12.png' },
+        { id: 13, value: 'public/assets/images/leo13.png'},
+        { id: 14, value: 'public/assets/images/leo14.png' },
+        { id: 15, value: 'public/assets/images/leo15.png' },
+        { id: 16, value: 'public/assets/images/leo16.png'},
+        { id: 17, value: 'public/assets/images/leo17.png' },
+        { id: 18, value: 'public/assets/images/leo18.png'},
     ];
 
     // Dupliquez et mélangez les cartes
@@ -95,7 +95,7 @@ function Card() {
                                 </div>
                                 <div className="card-back">
                                     {/* Insertion d'un GIF */}
-                                    <img src="images/metaverse.gif" alt="GIF" />
+                                    <img src="public/assets/images/metaverse.gif" alt="GIF" />
                                 </div>
                             </div>
                         </div>
